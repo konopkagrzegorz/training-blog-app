80a2d7ed0ebf630acbf3cb08e84837e3f6a7b19d
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-03 15:35:48 +0200

    Initial commit

---
+++ src/main/java/com/training/trainingblogapp/TrainingBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.trainingblogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingBlogAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/trainingblogapp/TrainingBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.trainingblogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
