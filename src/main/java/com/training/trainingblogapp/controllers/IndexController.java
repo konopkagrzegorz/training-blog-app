445f878bc47466a476bd0c51e9a73fd1c5a6e946
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2020-10-11 20:26:42 +0200

    Implemented thymeleaf

--- src/main/java/com/training/trainingblogapp/controllers/IndexController.java
+++ src/main/java/com/training/trainingblogapp/controllers/IndexController.java
@@ -5,10 +5,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping({"","/"})
 public class IndexController {
 
-    @GetMapping({"", "/", "index", "index.html"})
+    @GetMapping({"index", "index.html"})
     public String showIndexPage() {
-        return "ind";
+        return "index";
+    }
+
+    @GetMapping({"login", "login.html"})
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping({"register", "register.html"})
+    public String showRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping({"about", "about.html"})
+    public String showAboutPage() {
+        return "about";
+    }
+
+    @GetMapping({"contact","contact.html"})
+    public String showContactPage() {
+        return "contact";
     }
 }
