1a88ba29c90adf01e5a58418a9713e598266a151
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2021-02-06 13:40:25 +0100

    Changed columnDefinition in user

--- src/main/java/com/training/trainingblogapp/domain/model/User.java
+++ src/main/java/com/training/trainingblogapp/domain/model/User.java
@@ -19,10 +19,10 @@ public class User {
     @Column(columnDefinition = "TEXT")
     private String username;
 
-    @Column(name = "first_name")
+    @Column(name = "first_name",columnDefinition = "TEXT")
     private String firstName;
 
-    @Column(name = "last_name")
+    @Column(name = "last_name",columnDefinition = "TEXT")
     private String lastName;
 
     @Column(columnDefinition = "TEXT")
